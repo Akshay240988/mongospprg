97816413b0a37f57c9bd9fc457403b725d034fe8
Akshay Patil <akshaypatil240988@gmail.com>
2021-04-27 14:35:39 +0530

    Initial commit

---
+++ src/main/java/com/spmongo/mongospprg/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.spmongo.mongospprg.controller;
+
+
+import com.spmongo.mongospprg.model.EmployeeInfo;
+import com.spmongo.mongospprg.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostMapping("/save")
+    public String saveEmp(@RequestBody EmployeeInfo employee){
+        employeeRepository.save(employee);
+        return "Employee saved";
+    }
+
+    @GetMapping("/getallemployees")
+    public List<EmployeeInfo> getEmployeeList(){
+        return employeeRepository.findAll();
+    }
+    @GetMapping("/getemp/{id}")
+    public Optional<EmployeeInfo> getEmployee(@PathVariable String id){
+        return employeeRepository.findById(id);
+    }
+
+    @GetMapping("/deletebyid/{id}")
+    public String  deleteById(@PathVariable String id){
+        employeeRepository.deleteById(id);
+        return "Employee deleted";
+    }
+}
